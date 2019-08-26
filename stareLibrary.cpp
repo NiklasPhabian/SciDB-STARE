7436373050686271c7aad651286504c0fa83e36d
Niklas <griessbaum@gmail.com>
2019-08-26 15:42:28 -0700

    merge

--- stareLibrary.cpp
+++ stareLibrary.cpp
@@ -30,47 +30,49 @@ enum
 
 STARE stareIndex;
 
+
 // Spatial
 static void stareFromLevelLatLon(const scidb::Value** args, scidb::Value* res, void* v) {
-    int     iarg       = 0;
-    float64 latDegrees = args[iarg++]->getDouble();
-    float64 lonDegrees = args[iarg++]->getDouble();
-    int64_t depth      = args[iarg++]->getInt64();
+    float64 latDegrees = args[0]->getDouble();
+    float64 lonDegrees = args[1]->getDouble();
+    int64_t resolution = args[2]->getInt64();
 
-    STARE_ArrayIndexSpatialValue id = stareIndex.ValueFromLatLonDegrees(latDegrees, lonDegrees, depth);
+    STARE_ArrayIndexSpatialValue id = stareIndex.ValueFromLatLonDegrees(latDegrees, lonDegrees, resolution);
     *(STARE_ArrayIndexSpatialValue*)res->data() = id;
 }
 
+
 // Temporal
-static void stareFromTimeStamp(const scidb::Value** args, scidb::Value* res, void* v) {
-    int iarg        = 0;
-    int year        = 2015;
-    int month       = 12;
-    int day         = 3;
-    int hour        = 4;
-    int minute      = 5;
-    int second      = 7;
-    int millisecond = 30;
-    int resolution  = 3;
-    int type        = 2;
-    STARE_ArrayIndexTemporalValue indexValue = stareIndex.ValueFromUTCDatetime(year, month, day, hour, minute, second, millisecond, resolution, type);
-    *(STARE_ArrayIndexTemporalValue*)res->data() = indexValue ;
+static void stareFromUTCDateTime(const scidb::Value** args, scidb::Value* res, void* v) {
+    struct tm tm;
+    resolution = args[0];
+    time_t dt = args[1]->getDateTime(); // SciDB understands time_t as seconds since UNIX epoch
+    gmtime_r(&dt, &tm);                 // gmtime_r converts to tm struct, which stores ...
+    tm.tm_year += 1900;                 // years since 1900
+    tm.tm_mon += 1;                     // and months 0-based, while STARE stores months 1-based
+
+    STARE_ArrayIndexTemporalValue indexValue = stareIndex.ValueFromUTCDatetime(tm.tm_year, tm.tm_mon, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, 0, resolution, 2);
+    *(STARE_ArrayIndexTemporalValue*)res->data() = indexValue;
+}
+
+static void convDateTime2TimeT(const scidb::Value** args, scidb::Value* res, void*) {
+    time_t dt = args[0]->getDateTime();
+    res->setUint64(static_cast<uint64_t>(dt));
 }
 
 
 // Converters
-static void spatialIndexValueToString (const scidb::Value** args, scidb::Value* res, void* v) {
-    STARE_ArrayIndexSpatialValue& id = *(STARE_ArrayIndexSpatialValue*)args[0]->data();
+static void spatialIndexValueToString (const scidb::Value** args, scidb::Value* res, void* v) {      
+    STARE_ArrayIndexSpatialValue& id = *(STARE_ArrayIndexSpatialValue*)args[0]->data();        
     LOG4CXX_INFO(stare::logger, "STARE idx: " << id);
-    res->setString(to_string(id));
+    res->setString(to_string(id));  
 }
 
-static void temporalIndexValueToString (const scidb::Value** args, scidb::Value* res, void* v) {
-    STARE_ArrayIndexTemporalValue& id = *(STARE_ArrayIndexTemporalValue*)args[0]->data();
+static void temporalIndexValueToString (const scidb::Value** args, scidb::Value* res, void* v) {      
+    STARE_ArrayIndexTemporalValue& id = *(STARE_ArrayIndexTemporalValue*)args[0]->data();        
     LOG4CXX_INFO(stare::logger, "STARE idx: " << id);
-    res->setString(to_string(id));
+    res->setString(to_string(id));  
 }
-
 
 REGISTER_CONVERTER(spatialIndexValue, string, EXPLICIT_CONVERSION_COST, spatialIndexValueToString);
 REGISTER_CONVERTER(temporalIndexValue, string, EXPLICIT_CONVERSION_COST, temporalIndexValueToString);
@@ -98,18 +100,22 @@ public:
     Type temporalIndexValueType("stare", sizeof(STARE)*8);  // size in bits
     _types.push_back(spatialIndexValueType);
     _types.push_back(temporalIndexValueType);
-
+   
     _functionDescs.push_back(FunctionDescription("stareFromLevelLatLon",
                                                 list_of(TID_INT64)(TID_DOUBLE)(TID_DOUBLE),
                                                 TypeId("int64"),
                                                 &stareFromLevelLatLon));
-
-    _functionDescs.push_back(FunctionDescription("stareFromTimeStamp",
+   
+    _functionDescs.push_back(FunctionDescription("stareFromUTCDateTime",
                                                 list_of(TID_INT64),
                                                 TypeId("int64"),
                                                 &stareFromTimeStamp));
-
-
+    
+    _functionDescs.push_back(FunctionDescription("convDateTime2TimeT",
+                                                list_of(TID_DATETIME),
+                                                TypeId("int64"),
+                                                &strareFromDateTime);
+   
     _errors[STARE_ERROR1] = "STARE construction error.";
     scidb::ErrorsLibrary::getInstance()->registerErrors("stare",&_errors);
   }
